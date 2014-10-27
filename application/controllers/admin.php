c271ba3fcd4796ff40555831242575ed15facb82
Xain-Khalid-Malik <xain.malik53@gmail.com>
2014-10-28 01:00:35 +0500

    Admin Panel Compelte and Drills Improvement

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -7,12 +7,30 @@ class admin extends CI_Controller
         $this->load->helper('asset');
     }
     
-    function index()
+    function student()
     {
         $data['page_title'] = 'G-Learning | Admin';
         $this->load->view('main_header',$data);
         $this->load->view('admin_home_view');
         $this->load->view('footer');
     }
+    
+    function teacher()
+    {
+        $data['page_title'] = 'G-Learning | Admin';
+        $this->load->view('main_header',$data);
+        $this->load->view('admin_Addteacher_view');
+        $this->load->view('footer');
+    }
+    
+    function passwordRequests()
+    {
+        $data['page_title'] = 'G-Learning | Admin';
+        $this->load->view('main_header',$data);
+        $this->load->view('password_requests_view');
+        $this->load->view('footer');
+    }
+    
+    
 }
 ?>
