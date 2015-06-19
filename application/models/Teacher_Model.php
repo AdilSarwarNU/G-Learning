07a1d534dee3896d776d739e47de1a205a7ce3f9
Adil Sarwar <adil.sarwar921@gmail.com>
2015-06-20 04:42:50 +0500

    Result Card Minor edits

--- application/models/Teacher_Model.php
+++ application/models/Teacher_Model.php
@@ -194,12 +194,16 @@ public function insert_new_assessment($assess_name,$total_marks,$drill_id,$teach
         $student = array();      
         $student_ids = array();
         
+//        foreach ($assessments->result() as $assess_id)
+//        {   
+//            foreach ($assess_id as $id)
+//                array_push($assessments_id,$id);
+//        }
+        
         foreach ($assessments->result() as $assess_id)
         {   
-            foreach ($assess_id as $id)
-                array_push($assessments_id,$id);
+                array_push($assessments_id,$assess_id->assessment_id);
         }
-        
         
         foreach ($assessments_id as $a)
         {
